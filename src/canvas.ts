ab8170d0b2bce852d93305e9dfc0ed7a2612ef4f
Martin Krauskopf <martin.krauskopf@gmail.com>
2022-04-22 20:28:31 +0200

    Extract `initializeCanvas` to `factory.ts`

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,108 +1,6 @@
-import drawDebugBox from './debugBox'
-import { checkDefined } from './preconditions'
-import type { CanvasContext, CanvasInfo, DrawFunction } from './types'
+import type { CanvasContext, CanvasInfo } from './types'
 
 const defaultWidth: number = 1
-
-function initializeCanvas(canvasElementOrId: string | HTMLCanvasElement, drawFunction: DrawFunction): CanvasInfo {
-  let loop: boolean = false
-  let debugBoxVisible: boolean = false
-
-  const canvas =
-    typeof canvasElementOrId === 'string'
-      ? (document.getElementById(canvasElementOrId) as HTMLCanvasElement)
-      : canvasElementOrId
-
-  const canvasContainer = checkDefined(
-    canvas.parentElement,
-    'canvas (id=' + canvasElementOrId + ') must have parent element'
-  )
-
-  const ctx = checkDefined(canvas.getContext('2d'), 'canvas context')
-
-  const adjustCanvas = (ci: CanvasInfo): CanvasInfo => {
-    // Lookup the size the browser is displaying the canvas.
-    const displayWidth = canvasContainer.clientWidth
-    const displayHeight = canvasContainer.clientHeight
-
-    // Check if the canvas is not the same size and possibly adjust.
-    if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
-      canvas.width = displayWidth
-      canvas.height = displayHeight
-      return {
-        ...ci,
-        width: displayWidth,
-        height: displayHeight,
-      }
-    }
-    return ci
-  }
-
-  function drawCanvas(ci: CanvasInfo) {
-    drawFunction(ci)
-    if (debugBoxVisible) {
-      drawDebugBox(ci)
-    }
-  }
-
-  let animationHandle: number
-  function doLoop() {
-    drawCanvas(ci)
-    if (loop) {
-      animationHandle = requestAnimationFrame(() => doLoop())
-    }
-  }
-
-  function startLoop() {
-    if (loop) {
-      console.warn('Attempt to start the loop, but the loop is already running.')
-    } else {
-      loop = true
-      doLoop()
-    }
-  }
-
-  function stopLoop() {
-    if (!loop) {
-      console.warn('Attempt to stop the loop, but no loop is not running.')
-    }
-    if (animationHandle == null) {
-      console.warn('Attempt to stop the loop, with undefined animationHandle.')
-    } else {
-      cancelAnimationFrame(animationHandle)
-    }
-    loop = false
-  }
-
-  let ci: CanvasInfo = {
-    canvas,
-    ctx,
-    width: 0,
-    height: 0,
-    redraw: () => drawCanvas(ci),
-    startLoop,
-    stopLoop,
-    destroy: () => {},
-    showDebugBox: () => {
-      debugBoxVisible = true
-    },
-    hideDebugBox: () => {
-      debugBoxVisible = false
-    },
-  }
-
-  ci = adjustCanvas(ci)
-  const resizeObserver = new ResizeObserver((_entries) => {
-    ci = adjustCanvas(ci)
-    drawCanvas(ci)
-  })
-  resizeObserver.observe(canvasContainer)
-  ci.destroy = () => {
-    resizeObserver.unobserve(canvasContainer)
-  }
-
-  return ci
-}
 
 function clear({ ctx, width, height }: CanvasInfo): void {
   ctx.save()
@@ -132,4 +30,4 @@ function fillRGB(ctx: CanvasContext, r: string, g: string, b: string): void {
   ctx.fill()
 }
 
-export { initializeCanvas, defaultWidth, clear, fill, fillRGB, fillCanvas, stroke }
+export { defaultWidth, clear, fill, fillRGB, fillCanvas, stroke }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { initializeCanvas, stroke, fill, fillRGB, fillCanvas } from './canvas'
+export { initializeCanvas } from './factory'
+export { stroke, fill, fillRGB, fillCanvas } from './canvas'
 export { circle, ellipse } from './ellipse'
 export { line } from './line'
 export type { CanvasInfo, DrawFunction, Color } from './types'
