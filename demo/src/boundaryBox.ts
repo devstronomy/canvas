3719445aeadb807c093b51b88478f540b9c3989d
Martin Krauskopf <martin.krauskopf@gmail.com>
2022-04-24 15:19:21 +0200

    Minors for boundary box

--- demo/src/boundaryBox.ts
+++ demo/src/boundaryBox.ts
@@ -6,11 +6,11 @@ function boundaryBox({ ctx, canvas, zoom }: CanvasInfo, boundary: number) {
     x: boundary - 1,
     y: boundary - 1,
     w: canvas.width - 2 * (boundary - 1),
-    h: canvas.height - 2 * boundary + 2,
+    h: canvas.height - 2 * (boundary - 1),
     color: 'rgb(24, 188, 254)',
     dashed: true,
     dashGap: 15,
-    lineWidth: 1 * (1 / zoom.level),
+    lineWidth: 1 * (1 / zoom.level), // ignore zoom level for the box
   })
 }
 
