a6f47b65b512a0185bb1d2ca194e9503a2f3cae9
Martin Krauskopf <martin.krauskopf@gmail.com>
2022-05-05 08:35:59 +0200

    Minor: use `hsl` for boundary box

--- demo/src/boundaryBox.ts
+++ demo/src/boundaryBox.ts
@@ -1,16 +1,18 @@
 import { rect } from 'Canvas/rect'
 import { CanvasInfo } from 'Canvas/types'
 
+import { hsl } from './tools'
+
 function boundaryBox(ci: CanvasInfo, boundary: number) {
   rect(ci.ctx, {
     x: boundary - 1,
     y: boundary - 1,
     w: ci.width - 2 * (boundary - 1),
     h: ci.height - 2 * (boundary - 1),
-    color: 'rgb(24, 188, 254)',
+    color: hsl(210),
     dashed: true,
     dashGap: 15,
-    lineWidth: 1 * (1 / ci.zoom.level), // ignore zoom level for the box
+    lineWidth: 1 / ci.zoom.level, // ignore zoom level for the box
   })
 }
 
