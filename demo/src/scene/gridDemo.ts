4c564231f36a8383000f2d371cd57bc147aba2e6
Martin Krauskopf <martin.krauskopf@gmail.com>
2022-05-06 10:32:29 +0200

    Trivial refactoring: lineBase

--- demo/src/scene/gridDemo.ts
+++ demo/src/scene/gridDemo.ts
@@ -9,33 +9,23 @@ function gridDemo(ci: CanvasInfo) {
   const scaledWidth = ci.scale(width)
   const scaledHeight = ci.scale(height)
   const gridGap = width / 7
+  const lineBase = {
+    color: 'white',
+    width: ci.scale(1),
+  }
 
   ci.ctx.translate(width / 2, height / 2)
 
   // horizontal lines
   const xStart = -scaledWidth / 2 + ((scaledWidth / 2) % gridGap)
   for (let x = xStart; x < scaledWidth / 2; x += gridGap) {
-    line(ci.ctx, {
-      x1: x,
-      y1: -scaledHeight / 2,
-      x2: x,
-      y2: scaledHeight / 2,
-      color: 'white',
-      width: ci.scale(1),
-    })
+    line(ci.ctx, { x1: x, y1: -scaledHeight / 2, x2: x, y2: scaledHeight / 2, ...lineBase })
   }
 
   // vertical lines
   const yStart = -scaledHeight / 2 + ((scaledHeight / 2) % gridGap)
   for (let y = yStart; y < scaledHeight / 2; y += gridGap) {
-    line(ci.ctx, {
-      x1: -scaledWidth / 2,
-      y1: y,
-      x2: scaledWidth,
-      y2: y,
-      color: 'white',
-      width: ci.scale(1),
-    })
+    line(ci.ctx, { x1: -scaledWidth / 2, y1: y, x2: scaledWidth, y2: y, ...lineBase })
   }
 
   circle(ci.ctx, {
